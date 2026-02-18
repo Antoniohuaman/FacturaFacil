b88706a09f1393aebabc14ebf3729f749aee4119
Antoniohuaman <a.huaman@contasiscorp.com>
2026-02-18 14:44:30 -0500

    POSTHOG

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
 import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
+import { PostHogProvider } from "@posthog/react";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -6,7 +8,19 @@ import { FeedbackProvider } from "./shared/feedback/FeedbackProvider";
 import { TenantProvider } from "./shared/tenant/TenantProvider";
 import { ProveedorAyudaGuiada } from "./shared/tour";
 
-createRoot(document.getElementById("root")!).render(
+const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY as string | undefined;
+const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST as string | undefined;
+
+if (posthogKey) {
+  posthog.init(posthogKey, {
+    ...(posthogHost ? { api_host: posthogHost } : {}),
+    autocapture: true,
+    capture_pageview: true,
+    capture_pageleave: true,
+  });
+}
+
+const AppTree = (
   <ThemeProvider>
     <FeedbackProvider>
       <ProveedorAyudaGuiada>
@@ -16,4 +30,12 @@ createRoot(document.getElementById("root")!).render(
       </ProveedorAyudaGuiada>
     </FeedbackProvider>
   </ThemeProvider>
+);
+
+createRoot(document.getElementById("root")!).render(
+  posthogKey ? (
+    <PostHogProvider client={posthog}>{AppTree}</PostHogProvider>
+  ) : (
+    AppTree
+  )
 );
