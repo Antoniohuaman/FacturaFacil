558cb6165bf38b928576644aa07755e2d61c9ac7
Antoniohuaman <a.huaman@contasiscorp.com>
2026-02-19 00:06:33 -0500

    Revert "POSTHOG"
    This reverts commit b88706a09f1393aebabc14ebf3729f749aee4119.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
 import { createRoot } from "react-dom/client";
-import posthog from "posthog-js";
-import { PostHogProvider } from "@posthog/react";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext";
@@ -8,19 +6,7 @@ import { FeedbackProvider } from "./shared/feedback/FeedbackProvider";
 import { TenantProvider } from "./shared/tenant/TenantProvider";
 import { ProveedorAyudaGuiada } from "./shared/tour";
 
-const posthogKey = import.meta.env.VITE_PUBLIC_POSTHOG_KEY as string | undefined;
-const posthogHost = import.meta.env.VITE_PUBLIC_POSTHOG_HOST as string | undefined;
-
-if (posthogKey) {
-  posthog.init(posthogKey, {
-    ...(posthogHost ? { api_host: posthogHost } : {}),
-    autocapture: true,
-    capture_pageview: true,
-    capture_pageleave: true,
-  });
-}
-
-const AppTree = (
+createRoot(document.getElementById("root")!).render(
   <ThemeProvider>
     <FeedbackProvider>
       <ProveedorAyudaGuiada>
@@ -30,12 +16,4 @@ const AppTree = (
       </ProveedorAyudaGuiada>
     </FeedbackProvider>
   </ThemeProvider>
-);
-
-createRoot(document.getElementById("root")!).render(
-  posthogKey ? (
-    <PostHogProvider client={posthog}>{AppTree}</PostHogProvider>
-  ) : (
-    AppTree
-  )
 );
